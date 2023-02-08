d15cfbc479a302b0e22df3fcd591226db86a7eb6
Aman Prasad <aprasad2707@gmail.com>
2023-02-08 20:47:52 +0530

    Adding project files

---
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webButton= findViewById<CardView>(R.id.cardweb)
+        val cameraButton= findViewById<CardView>(R.id.cardcamera)
+
+
+        webButton.setOnClickListener{
+            val intent= Intent(Intent.ACTION_VIEW)
+            intent.data= Uri.parse("https://www.linkedin.com/in/aman-prasad-90478b230/")
+            startActivity(intent)
+        }
+
+        cameraButton.setOnClickListener{
+            val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+
+        }
+    }
+
+}
